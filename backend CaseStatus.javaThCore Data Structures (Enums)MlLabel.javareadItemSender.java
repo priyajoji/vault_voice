d2a3eb8629265f1d93bae627466cecd9038e851b
jagruthi50 <jagruthisuneel@gmail.com>
2025-09-28 19:22:57 +0530

    Create backend CaseStatus.javaThCore Data Structures (Enums)MlLabel.javareadItemSender.java

---
+++ backend CaseStatus.javaThCore Data Structures (Enums)MlLabel.javareadItemSender.java
@@ -0,0 +1,22 @@
+// CaseStatus.java
+package com.vaultvoice.enums;
+public enum CaseStatus {
+    NEW,
+    IN_REVIEW,
+    CLOSED
+}
+
+// ThreadItemSender.java
+package com.vaultvoice.enums;
+public enum ThreadItemSender {
+    WHISTLEBLOWER,
+    INVESTIGATOR
+}
+
+// MlLabel.java
+package com.vaultvoice.enums;
+public enum MlLabel {
+    ABUSIVE,
+    NON_ABUSIVE,
+    UNCATEGORIZED
+}
